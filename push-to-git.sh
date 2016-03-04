d690bda3473d041f61d099302a007282c471fe81
Jon Aumann <jaumann@millennialmedia.com>
2016-03-04 13:35:10 -0500

    daily crontab backup Fri Mar  4 13:35:10 EST 2016

--- push-to-git.sh
+++ push-to-git.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-cd /Users/jaumann/dotfiles && git add . && git commit -a -m "daily crontab backup `date`" && git push
+cd /Users/jaumann/dotfiles && git add . && git commit -a -m "daily crontab backup `date`" 
+cd /Users/jaumann/dotfiles && git push
