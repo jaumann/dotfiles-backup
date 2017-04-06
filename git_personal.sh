091da620712020937bdb76df18b4361abf2bf505
Jon Aumann <jaumann@gmail.com>
2017-04-06 13:18:45 -0400

    Adding personal git config

---
+++ git_personal.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config user.name "Jon Aumann"
+git config user.email "jaumann@gmail.com"
+git config user.signingkey "AA047095"
+git config gpg.program "gpg2"
+git config commit.gpgsign "true"
