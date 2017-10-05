9c3aea997061938feeafde6c36ae518f9e4d8b26
Jon Aumann <jaumann@gmail.com>
2017-10-05 08:54:45 -0400

    daily crontab backup Thu Oct  5 08:54:45 EDT 2017

--- routes.sh
+++ routes.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-route add -net 192.168.30/24 -gateway 10.23.224.1
-route add -net 192.168.40/24 -gateway 10.23.224.1
-route add -net 192.168.140/24 -gateway 10.23.224.1
-route add -net 192.168.130/24 -gateway 10.23.224.1
+ROUTES=(184.106.43/24 173.203.167/24 192.168.30/24 192.168.40/24 192.168.140/24 192.168.130/24)
+
+if [ "$1" != "add" ] && [ "$1" != "delete" ] ; then
+  echo "Need to specify add or delete"
+  exit 1
+fi
+
+for i in ${ROUTES[@]}; do
+  route $1 -net $i -gateway 10.23.224.1
+done
+
